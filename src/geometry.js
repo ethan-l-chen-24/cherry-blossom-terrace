df67eafaccc1e6daf96b646bf40004045c36db19
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2024-02-27 17:02:22 -0500

    basic particle system

--- src/geometry.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from 'three';
-
-export function addGeometry(scene) {
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshBasicMaterial( { color: 0xFFFFFF } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-}
