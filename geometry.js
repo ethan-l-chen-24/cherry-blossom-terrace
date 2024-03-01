ed7555ef960ef2694f914dcd1772d808eac21307
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2024-02-29 20:29:38 -0500

    added ground collision, orientation, and gusts

--- geometry.js
+++ geometry.js
@@ -6,3 +6,14 @@ export function addGeometry(scene) {
     const cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
 }
+
+export function addFloor(scene) {
+    var geometry = new THREE.PlaneGeometry(2000, 2000, 10, 10);
+    var material = new THREE.MeshBasicMaterial({ color: 0x333333, side: THREE.DoubleSide });
+    var ground = new THREE.Mesh(geometry, material);
+    ground.rotation.x = -Math.PI / 2; 
+    ground.position.y = 0;
+
+    // Add the ground to the scene
+    scene.add(ground);
+}
